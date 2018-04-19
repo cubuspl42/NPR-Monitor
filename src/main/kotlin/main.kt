e800196b2f3c5bdeaccdcf30fe0a8d3d7e949a03
cubuspl42 <cubuspl42@gmail.com>
2018-04-20 00:21:00 +0200

    Initial (partially) working implementation

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,291 @@
+import MessageType.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import kotlinx.serialization.Serializable
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.Semaphore
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.concurrent.thread
+
+
+typealias NodeId = Int
+
+enum class MessageType {
+    REQUEST,
+    RESPONSE,
+    RELEASE,
+    WAIT,
+    NOTIFY
+}
+
+@Serializable
+data class Message(
+        val type: MessageType,
+        val timestamp: Int,
+        val nodeId: NodeId,
+        val conditionId: Int = 0
+)
+
+data class Request(
+        val timestamp: Int,
+        val nodeId: NodeId
+) : Comparable<Request> {
+    override fun compareTo(other: Request) = compareValuesBy(this, other,
+            { it.timestamp },
+            { it.nodeId }
+    )
+}
+
+val nodeAddresses = listOf(
+        InetSocketAddress("localhost", 5555),
+        InetSocketAddress("localhost", 5556),
+        InetSocketAddress("localhost", 5557),
+        InetSocketAddress("localhost", 5558)
+)
+
+val mapper = jacksonObjectMapper()
+
+class Cluster(
+        private val thisNodeId: NodeId,
+        private val distributedLock: DistributedLock,
+        private val socketMap: Map<NodeId, Socket>
+) {
+    private val messageQueue = ArrayBlockingQueue<Message>(8)
+
+    init {
+        if (thisNodeId !in nodeAddresses.indices) throw AssertionError()
+
+        socketMap.values.map { socket ->
+            thread {
+                val scanner = Scanner(socket.getInputStream()).apply {
+                    useDelimiter("\n")
+                }
+                while (true) {
+                    val messageString = scanner.next()
+                    val message = mapper.readValue<Message>(messageString)
+                    messageQueue.put(message)
+                }
+            }
+        }
+
+        thread {
+            println("Receiving loop...")
+            while (true) {
+                val message = messageQueue.take()
+                distributedLock.handleMessage(message)
+            }
+        }
+    }
+
+
+    internal fun send(nodeId: NodeId, message: Message) {
+        if (nodeId == thisNodeId) {
+            distributedLock.handleMessage(message)
+        } else {
+            val socket = socketMap[nodeId]!!
+            send(socket, message)
+        }
+    }
+
+    internal fun broadcast(message: Message) {
+        distributedLock.handleMessage(message)
+        socketMap.values.forEach { socket ->
+            send(socket, message)
+        }
+    }
+
+    private fun send(socket: Socket, message: Message) {
+        println("Sending message to ${socket.remoteSocketAddress}: $message")
+        socket.getOutputStream().run {
+            write(mapper.writeValueAsBytes(message))
+            write("\n".toByteArray())
+        }
+    }
+
+    val size = nodeAddresses.size
+}
+
+private fun buildServerSocket(thisNodeId: NodeId): ServerSocket {
+    val port = nodeAddresses[thisNodeId].port
+    return ServerSocket(port)
+}
+
+private fun buildSocketMap(thisNodeId: NodeId, serverSocket: ServerSocket): Map<NodeId, Socket> {
+    val lowerNodesSockets = connectToNodes(thisNodeId)
+    val higherNodesSockets = acceptNodesConnections(thisNodeId, serverSocket)
+    return (lowerNodesSockets + higherNodesSockets).toMap()
+}
+
+private fun connectToNodes(thisNodeId: NodeId): List<Pair<NodeId, Socket>> =
+        (0 until thisNodeId).map { nodeId ->
+            val socketAddress = nodeAddresses[nodeId]
+            println("Connecting to $socketAddress...")
+            nodeId to Socket(socketAddress.address, socketAddress.port)
+        }
+
+private fun acceptNodesConnections(thisNodeId: NodeId, serverSocket: ServerSocket): List<Pair<NodeId, Socket>> =
+        (thisNodeId + 1 until nodeAddresses.size).map { nodeId ->
+            val socket = serverSocket.accept()
+
+            println("${socket.remoteSocketAddress} connected!")
+
+            nodeId to socket
+        }
+
+fun Cluster(
+        thisNodeId: NodeId,
+        distributedLock: DistributedLock
+): Cluster {
+    val serverSocket = buildServerSocket(thisNodeId)
+
+    println("Bound address: ${serverSocket.localSocketAddress}")
+
+    val socketMap = buildSocketMap(thisNodeId, serverSocket)
+
+    println("Socket map:")
+    socketMap.forEach { nodeId, socket ->
+        println("$nodeId: ${socket.localSocketAddress} -> ${socket.remoteSocketAddress}")
+    }
+
+    return Cluster(thisNodeId, distributedLock, socketMap)
+}
+
+
+class DistributedLock(
+        private val thisNodeId: NodeId
+) {
+    private val cluster = Cluster(thisNodeId, this)
+
+    private var time = 0
+
+    private val semaphore = Semaphore(0)
+
+    private val queue: Queue<Request> = PriorityQueue()
+
+    private val conditions = mutableListOf<DistributedCondition>()
+
+    private var responsesNeeded = 0
+
+    fun acquire() {
+        requestLock()
+        sleep()
+    }
+
+    @Synchronized
+    fun release() {
+        cluster.broadcast(Message(RELEASE, ++time, thisNodeId))
+    }
+
+    internal fun sleep() {
+        semaphore.acquire()
+    }
+
+    private fun wakeup() {
+        if (semaphore.availablePermits() != 0) throw AssertionError()
+        semaphore.release()
+    }
+
+    @Synchronized
+    private fun requestLock() {
+        responsesNeeded = cluster.size
+        cluster.broadcast(Message(REQUEST, ++time, thisNodeId))
+    }
+
+    @Synchronized
+    internal fun handleMessage(message: Message) {
+        println("Received message: $message")
+
+        time = Math.max(time, message.timestamp) + 1
+
+        when (message.type) {
+            REQUEST -> {
+                queue.add(Request(message.timestamp, message.nodeId))
+                cluster.send(message.nodeId, Message(RESPONSE, ++time, thisNodeId))
+            }
+            RESPONSE -> {
+                if (responsesNeeded <= 0) throw AssertionError()
+
+                --responsesNeeded
+                if (responsesNeeded == 0) {
+                    tryEnteringCriticalSection()
+                }
+            }
+            RELEASE -> {
+                onRelease(message)
+            }
+            WAIT -> {
+                conditions[message.conditionId].queue.add(Request(message.timestamp, message.nodeId))
+                onRelease(message)
+            }
+            NOTIFY -> {
+                if (queue.firstOrNull()?.nodeId != message.nodeId) throw AssertionError()
+                val request = conditions[message.conditionId].queue.poll()
+                queue.add(request)
+            }
+        }
+
+        println("Queue: $queue")
+    }
+
+    private fun onRelease(message: Message) {
+        if (queue.firstOrNull()?.nodeId != message.nodeId) throw AssertionError()
+        queue.remove()
+        tryEnteringCriticalSection()
+    }
+
+    private fun tryEnteringCriticalSection() {
+        if (queue.firstOrNull()?.nodeId == thisNodeId) {
+            wakeup()
+        }
+    }
+}
+
+class DistributedCondition(
+        private val distributedLock: DistributedLock,
+        private val conditionId: Int,
+        private val cluster: Cluster
+) {
+    internal val queue: Queue<Request> = PriorityQueue()
+
+    fun wait_() {
+        cluster.broadcast(Message(WAIT, -1, -1, conditionId))
+        distributedLock.sleep()
+    }
+
+    fun notify_() {
+        cluster.broadcast(Message(NOTIFY, -1, -1, conditionId))
+    }
+}
+
+fun rand(range: ClosedRange<Int>) =
+        ThreadLocalRandom.current().nextInt(range.endInclusive - range.start) + range.start
+
+fun sleep(millis: Int) {
+    println("Sleeping for $millis...")
+    Thread.sleep(millis.toLong())
+}
+
+fun sleep(range: ClosedRange<Int>) {
+    sleep(rand(range))
+}
+
+fun main(args: Array<String>) {
+    val thisNodeId = args[0].toInt()
+
+    val mutex = DistributedLock(thisNodeId)
+
+    while (true) {
+        sleep(1000..60000)
+        println("About to acquire mutex...")
+        mutex.acquire()
+        println("Entered critical section!")
+        sleep(1000..5000)
+        println("About to release mutex...")
+        mutex.release()
+        println("Left critical section!")
+    }
+}
